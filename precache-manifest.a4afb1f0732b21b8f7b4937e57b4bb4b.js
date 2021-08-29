a83eca5e13b6bfcbc4b3992b7e6faf382c27c304
Josh Guo <joshua.guo@stonybrook.edu>
2021-08-29 11:48:43 -0400

    Updates

---
+++ precache-manifest.a4afb1f0732b21b8f7b4937e57b4bb4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c4eac766dcfdd29fbf347d36d5c0c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63c9df7e8805cd0bd3fe",
+    "url": "/static/css/main.43160952.chunk.css"
+  },
+  {
+    "revision": "a8c4a9b6030ddf3a43ef",
+    "url": "/static/js/2.3178290e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3178290e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63c9df7e8805cd0bd3fe",
+    "url": "/static/js/main.c06e4f88.chunk.js"
+  },
+  {
+    "revision": "78cfa3f6ed15fa84a0cf",
+    "url": "/static/js/runtime-main.c7640cd5.js"
+  },
+  {
+    "revision": "092e6056e9799fdbbadf8331ade8e1a9",
+    "url": "/static/media/kaitlynchua.092e6056.jpg"
+  }
+]);
